f4a94a80bd68cd9121585bd1dd64963c83278f93
Daniel <dh99@live.se>
2021-11-17 09:34:58 +0100

    Implemented CalendarInputSelect generic + roadmap to develop

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div>
-    
+    <calendar />
   </div>
 </template>
 
 <script>
+import Calendar from "./components/Calendar.vue";
+
 export default {
+  components: { Calendar },
   name: "App",
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/CalendarInputSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <select class="select">
+    <option
+      v-for="(option, i) of options"
+      :key="`${option}+${i}`"
+      :value="i"
+    >
+      {{ option }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      required: true,
+      type: Array,
+    },
+  },
+  methods: {
+    onSelect() {
+      
+    }
+  }
+};
+</script>
+
+<style scoped>
+.select {
+}
+</style>
